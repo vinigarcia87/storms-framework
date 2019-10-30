0c57afd7dbb54b6eeced114db23e8805c27a7386
Vinicius Garcia <vinigarcia87@gmail.com>
2019-10-30 17:46:58 -0300

    - comentario

--- storms-framework.php
+++ storms-framework.php
@@ -7,7 +7,6 @@
  * Copyright: (c) Copyright 2012-2016, Storms Websolutions
  * License: GPLv2 - GNU General Public License v2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
  * Version: 0.1
- *
  * Text Domain: storms
  * Domain Path: /i18n/languages/
  */
