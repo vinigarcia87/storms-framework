5eaa781426395264457ca32a2b283054279d10fd
Vinicius Garcia <vinigarcia87@gmail.com>
2020-06-24 15:14:37 -0300

    - Layout padrao para o checkout, com option

--- lib/StormsFramework/Template.php
+++ lib/StormsFramework/Template.php
@@ -277,10 +277,16 @@ class Template extends Base\Runner
 		if( 'default' === $layout ) {
 			if( is_front_page() ) {
 				$layout = Helper::get_option( 'storms_front_page_layout', '1c' );
+
 			}elseif( is_product() ) {
 				$layout = Helper::get_option( 'storms_product_layout', '1c' );
+
 			}elseif( is_account_page() ) {
 				$layout = Helper::get_option( 'storms_account_layout', '1c' );
+
+			}elseif( is_checkout() ) {
+				$layout = Helper::get_option( 'storms_checkout_layout', '1c' );
+
 			}elseif( is_shop() || is_product_category() || is_product_tag() ) {
 				$layout = Helper::get_option( 'storms_shop_layout', '2c-l' );
 
