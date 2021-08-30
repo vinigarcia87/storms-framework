270489c39b8b5b9323f3c1104fb75f74ce4344b1
Vinicius Garcia <vinigarcia87@gmail.com>
2021-08-30 17:01:20 -0300

    Regenerating assets

--- assets/js/storms-wc-mini-cart.js
+++ assets/js/storms-wc-mini-cart.js
@@ -45,7 +45,9 @@ body.addEventListener( 'click', function( event ) {
 
 	// The click was outside the Mini Cart...
 	var dropdown = document.querySelector( '.storms-cart-contents .shopping_cart_dropdown' );
-	// The Mini Cart was open, so we going to close it
-	dropdown.classList.remove( 'active' );
+	if( dropdown ) {
+		// The Mini Cart was open, so we going to close it
+		dropdown.classList.remove( 'active' );
+	}
 
 } );
